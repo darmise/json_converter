d9e97da3bb9bac7acfdcdcd8342060a4dee2ec7d
darmise <74202928+darmise@users.noreply.github.com>
2025-12-13 12:36:58 +0100

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -11,4 +11,4 @@ class Utils:
             
     def save_json(self, json_data, output):
         with open(output, "w", encoding="utf-8") as f:
-            json.dump(json_data, f, indent=2, ensure_ascii=False)
+            json.dump(json_data, f, indent=4, ensure_ascii=False)
