f95da883cbf9befa505bd4b9e7699dccb6d11d81
darmise <74202928+darmise@users.noreply.github.com>
2026-01-02 11:17:12 +0100

    Editable

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="json_converter",           # nome del pacchetto
+    name="json_converter",          # nome del pacchetto
     version="0.1",
-    packages=find_packages(where="src"),  # cerca pacchetti dentro src
-    package_dir={"": "src"},         # mappa la root dei pacchetti a src
+    packages=find_packages(where="src"),  
+    package_dir={"": "src"}
 )
+
